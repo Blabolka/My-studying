969a8abf1f9c54772b7406fa23844a1650b819ef
Blabolka <bida.andrey01@gmail.com>
2020-10-15 16:32:36 +0300

    Work with BufferedReader and BufferedWriter

--- Studying/OOP/LABS/LAB2/src/Main.java
+++ Studying/OOP/LABS/LAB2/src/Main.java
@@ -2,10 +2,20 @@ public class Main {
 
     public static void main(String[] args){
 
-        ModifyFile modifyFile = new ModifyFile("Digital values.txt", "Modified Digit Values.txt");
+        ModifyFile modifyFile1 = new ModifyFile("Digital values.txt", "Modified Digit Values.txt");
 
-        modifyFile.clearFile();
-        modifyFile.splitStringIntoSentences();
-        modifyFile.splitStringIntoWords();
+        modifyFile1.clearFile();
+        modifyFile1.copyContent();
+        modifyFile1.splitStringIntoSentences();
+        modifyFile1.splitStringIntoWords();
+        modifyFile1.countNumberOfWords();
+        modifyFile1.countNumberOfSymbols();
+        modifyFile1.changeFirstSybolOfEveryWordToUppercase();
+
+        ModifyFile modifyFile2 = new ModifyFile("Flyby missions.txt", "Modified Flyby missions.txt");
+        modifyFile2.clearFile();
+        modifyFile2.copyContent();
+        modifyFile2.splitStringIntoWords();
+        modifyFile2.countNumberOfSymbols();
     }
 }
--- Studying/OOP/LABS/LAB2/src/ModifyFile.java
+++ Studying/OOP/LABS/LAB2/src/ModifyFile.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.ArrayList;
 
 public class ModifyFile {
 
@@ -10,8 +11,22 @@ public class ModifyFile {
         fileToWrite = new File(fileWriterName);
     }
 
-    void splitStringIntoSentences() {
+    void copyContent(){
+        try(BufferedReader br = new BufferedReader(new FileReader(fileToRead));
+            BufferedWriter bw = new BufferedWriter(new FileWriter(fileToWrite, true))) {
+            bw.write("КОПИРОВАНИЕ КОНТЕНТА ФАЙЛА" + System.lineSeparator());
 
+            String line;
+            while ((line = br.readLine()) != null){
+                bw.write(line + System.lineSeparator());
+            }
+            bw.write(System.lineSeparator());
+
+        }catch (IOException ex){
+        }
+    }
+
+    void splitStringIntoSentences() {
 
         try(BufferedReader br = new BufferedReader(new FileReader(fileToRead));
             BufferedWriter bw = new BufferedWriter(new FileWriter(fileToWrite, true))) {
@@ -32,35 +47,82 @@ public class ModifyFile {
             bw.write(sb.toString() + System.lineSeparator());
 
         }catch (IOException ex){
-            ex.printStackTrace();
         }
     }
 
     void splitStringIntoWords(){
 
-        try(BufferedReader br = new BufferedReader(new FileReader(fileToRead));
-            BufferedWriter bw = new BufferedWriter(new FileWriter(fileToWrite, true))){
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(fileToWrite, true))){
             bw.write("РАЗБИЕНИЕ ТЕКСТА НА СЛОВА И СЛОВОСОЧЕТАНИЯ" + System.lineSeparator());
 
-            String line;
-            StringBuffer sb = new StringBuffer();
-            while( (line = br.readLine()) != null){
-                for (int i = 0; i < line.length(); i++) {
-                    if(line.charAt(i) == ' ' || line.charAt(i) == '.' || line.charAt(i) == ','){
-                        if(sb.length() != 0){
-                            bw.write(sb.toString() + System.lineSeparator());
-                            sb.setLength(0);
-                        }
+            ArrayList<String> arrayList = getAllWordsFromFile();
 
-                    }else{
-                        sb.append(line.charAt(i));
+            for(int i=0 ; i < arrayList.size() ; i++){
+                bw.write(arrayList.get(i) + System.lineSeparator());
+            }
+
+        }catch (IOException ex){
+        }
+    }
+
+    void countNumberOfWords(){
+
+        try(BufferedReader br = new BufferedReader(new FileReader(fileToRead));
+            BufferedWriter bw = new BufferedWriter(new FileWriter(fileToWrite, true))){
+
+            bw.write("ПОДСЧЁТ КОЛИЧЕСТВА СЛОВ" + System.lineSeparator());
+
+            int counter = 0;
+            String line;
+            while( (line = br.readLine()) != null ){
+                for(int i=0 ; i<line.length() ; i++){
+                    if(line.charAt(i) == ' '){
+                        counter++;
                     }
                 }
             }
-            bw.write(sb.toString() + System.lineSeparator());
+            bw.write("КОЛИЧЕСТВО СЛОВ: " + counter + System.lineSeparator() + System.lineSeparator());
 
-        }catch (IOException ex){
-            ex.printStackTrace();
+        }catch(IOException ex){
+
+        }
+    }
+
+    void countNumberOfSymbols(){
+
+        try(BufferedReader br = new BufferedReader(new FileReader(fileToRead));
+            BufferedWriter bw = new BufferedWriter(new FileWriter(fileToWrite, true))){
+
+            bw.write("ПОДСЧЁТ КОЛИЧЕСТВА СИМВОЛОВ" + System.lineSeparator());
+
+            int counter = 0;
+            String line;
+            while( (line = br.readLine()) != null ){
+                counter += line.length();
+            }
+            bw.write("КОЛИЧЕСТВО СИМВОЛОВ: " + counter + System.lineSeparator() + System.lineSeparator());
+
+        }catch(IOException ex){
+
+        }
+    }
+
+    void changeFirstSybolOfEveryWordToUppercase(){
+
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(fileToWrite, true))){
+
+            bw.write("ЗАМЕНА ВСЕХ ПЕРВЫХ СТРОЧНЫХ БУКВ В СЛОВАХ НА ПРОПИСНУЮ" + System.lineSeparator());
+
+            ArrayList<String> arrayList = getAllWordsFromFile();
+
+            for (int i = 0; i < arrayList.size(); i++) {
+                if(arrayList.get(i) != null || !arrayList.get(i).equals("")){
+                    arrayList.set(i, arrayList.get(i).substring(0,1).toUpperCase() + arrayList.get(i).substring(1));
+                    bw.write(arrayList.get(i) + " ");
+                }
+            }
+
+        }catch(IOException ex){
         }
     }
 
@@ -70,6 +132,36 @@ public class ModifyFile {
         }catch (IOException ex){
             ex.printStackTrace();
         }
+    }
+
+    private ArrayList<String> getAllWordsFromFile(){
+
+        ArrayList<String> arrayList = new ArrayList<>();
+
+        try(BufferedReader br = new BufferedReader(new FileReader(fileToRead))){
+
+            String line;
+            StringBuffer sb = new StringBuffer();
+            while( (line = br.readLine()) != null){
+                for (int i = 0; i < line.length(); i++) {
+                    if(line.charAt(i) == ' '){
+                        if(sb.length() != 0){
+                            arrayList.add(sb.toString());
+                            sb.setLength(0);
+                        }
+                    }else{
+                        sb.append(line.charAt(i));
+                    }
+                }
+                sb.append(System.lineSeparator());
+                arrayList.add(sb.toString());
+                sb.setLength(0);
+            }
+
+        }catch (IOException ex){
+        }
+
+        return arrayList;
     }
 
 }
