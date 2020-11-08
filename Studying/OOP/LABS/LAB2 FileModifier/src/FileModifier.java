bdbfd7c10fcbaa59a1844e46640badaf475a4ae7
Blabolka <klochko.andrew.official@gmail.com>
2020-11-08 23:37:40 +0200

    Updated LAB2

--- Studying/OOP/LABS/LAB2 FileModifier/src/FileModifier.java
+++ Studying/OOP/LABS/LAB2 FileModifier/src/FileModifier.java
@@ -1,8 +1,9 @@
+import org.apache.commons.lang3.CharUtils;
 import org.apache.commons.lang3.StringUtils;
 
 public class FileModifier {
 
-    public static final String NEXT_LINE = System.lineSeparator();
+    private static final String NEXT_LINE = System.lineSeparator();
 
     FileReader fileReader;
     FileWriter fileWriter;
@@ -28,35 +29,106 @@ public class FileModifier {
 
         StringBuilder splitStringBuilder = new StringBuilder();
         for (String s : splitString) {
-            s = StringUtils.strip(s);
             splitStringBuilder.append(s).append(NEXT_LINE);
         }
         addBodyToFileForWriting(splitStringBuilder.toString());
     }
 
     public void splitIntoWords(){
-        addHeaderToFileForWriting("SPLIT STRING INTO WORDS");
+        addHeaderToFileForWriting("SPLIT INTO WORDS");
 
-        String[] allWordsFromText = StringUtils.split(allTextFromFileForReading.toString(), "\t ,.");
+        String[] wordsWithoutSymbols = getAllWordsFromTextWithoutSymbols(allTextFromFileForReading.toString());
 
-        StringBuilder allWordsFromTextBuider = new StringBuilder();
-        for (String s : allWordsFromText) {
-            allWordsFromTextBuider.append(s).append(NEXT_LINE);
+        StringBuilder wordsWithoutSymbolsStringBuider = new StringBuilder();
+        for (String w : wordsWithoutSymbols) {
+            wordsWithoutSymbolsStringBuider.append(w).append(NEXT_LINE);
         }
-        addBodyToFileForWriting(allWordsFromTextBuider.toString());
+        addBodyToFileForWriting(wordsWithoutSymbolsStringBuider.toString());
     }
 
-    public void countAllWords(){
-        addHeaderToFileForWriting("NUMBER OF WORDS");
-        addBodyToFileForWriting(getAllWordsFromFile().length + NEXT_LINE);
+    public void countTheNumberOfWords(){
+        addHeaderToFileForWriting("COUNT OF WORDS");
+        addBodyToFileForWriting(getAllWordsFromTextWithoutSymbols(allTextFromFileForReading.toString()).length + NEXT_LINE);
+    }
+
+    public void countTheNumberOfCharacters(){
+        addHeaderToFileForWriting("COUNT OF CHARACTERS");
+        addBodyToFileForWriting(allTextFromFileForReading.toString().length() + NEXT_LINE);
+    }
+
+    public void capitalizeEveryWord(){
+        addHeaderToFileForWriting("CAPITALIZE EVERY WORD");
+
+        String[] wordsWithoutWhitespace = getAllWordsFromTextWithoutWhitespace();
+
+        StringBuilder wordsWithoutWhitespaceStringBuilder = new StringBuilder();
+        for (String s : wordsWithoutWhitespace) {
+            wordsWithoutWhitespaceStringBuilder.append(StringUtils.capitalize(s)).append(" ");
+        }
+        addBodyToFileForWriting(wordsWithoutWhitespaceStringBuilder.toString());
+    }
+
+    public void toUpperCaseEverySecondWord(){
+        addHeaderToFileForWriting("TO UPPER CASE EVERY SECOND WORD");
+
+        String[] wordsWithoutWhitespace = getAllWordsFromTextWithoutWhitespace();
+
+        StringBuilder textWithUpperCase = new StringBuilder();
+        for (int i = 1; i <= wordsWithoutWhitespace.length; i++) {
+            if(i%2 == 0){
+                textWithUpperCase.append(wordsWithoutWhitespace[i-1].toUpperCase()).append(" ");
+            }else{
+                textWithUpperCase.append(wordsWithoutWhitespace[i-1]).append(" ");
+            }
+        }
+        addBodyToFileForWriting(textWithUpperCase.toString());
+    }
+
+    public void createNewWordsFromTwoFirstAndLastLettersEveryWord(){
+        addHeaderToFileForWriting("WORDS FROM TWO FIRST AND LAST LETTERS EVERY WORD");
+
+        String[] wordsWithoutWhitespace = getAllWordsFromTextWithoutWhitespace();
+
+        StringBuilder newWords = new StringBuilder();
+        for (int i = 0; i < wordsWithoutWhitespace.length; i++) {
+            String word = getStringWithoutSymbols(wordsWithoutWhitespace[i]);
+            if(word.length() > 4){
+                String newWord = word.substring(0,2) + word.substring(word.length()-2);
+                wordsWithoutWhitespace[i] = StringUtils.replace(wordsWithoutWhitespace[i], word, newWord);
+            }
+            newWords.append(wordsWithoutWhitespace[i]).append(" ");
+        }
+        addBodyToFileForWriting(newWords.toString());
     }
 
     public void writeToFile(){
         fileWriter.writeTextToFile(allTextForFileForWriting.toString());
     }
 
-    private String[] getAllWordsFromFile(){
-        return StringUtils.split(allTextFromFileForReading.toString(), "\t ,.");
+    private String[] getAllWordsFromTextWithoutSymbols(String text){
+        StringBuilder separator = new StringBuilder();
+        for (int i = 0; i < text.length() ; i++) {
+            if(!CharUtils.isAsciiAlpha(text.charAt(i)) && !CharUtils.isAsciiNumeric(allTextFromFileForReading.toString().charAt(i))){
+                if(!separator.toString().contains(String.valueOf(text.charAt(i)))){
+                    separator.append(text.charAt(i));
+                }
+            }
+        }
+        return StringUtils.split(text, separator.toString());
+    }
+
+    private String[] getAllWordsFromTextWithoutWhitespace(){
+        return StringUtils.split(allTextFromFileForReading.toString(), " \t");
+    }
+
+    private String getStringWithoutSymbols(String string){
+        StringBuilder newWord = new StringBuilder();
+        for (int i = 0; i < string.length(); i++) {
+            if(Character.isLetter(string.charAt(i))){
+                newWord.append(string.charAt(i));
+            }
+        }
+        return newWord.toString();
     }
 
     private void addHeaderToFileForWriting(String header){
--- Studying/OOP/LABS/LAB2 FileModifier/src/Main.java
+++ Studying/OOP/LABS/LAB2 FileModifier/src/Main.java
@@ -4,12 +4,18 @@ public class Main {
         fileModifier1.copyAllText();
         fileModifier1.splitBySeparator(".");
         fileModifier1.splitIntoWords();
-        fileModifier1.countAllWords();
+        fileModifier1.countTheNumberOfWords();
+        fileModifier1.countTheNumberOfCharacters();
+        fileModifier1.capitalizeEveryWord();
+        fileModifier1.toUpperCaseEverySecondWord();
+        fileModifier1.createNewWordsFromTwoFirstAndLastLettersEveryWord();
         fileModifier1.writeToFile();
 
         FileModifier fileModifier2 = new FileModifier("Flyby missions.txt", "Modified Flyby missions.txt");
-        fileModifier2.splitBySeparator("");
+        fileModifier2.copyAllText();
         fileModifier2.splitIntoWords();
+        fileModifier2.countTheNumberOfWords();
+        fileModifier2.countTheNumberOfCharacters();
         fileModifier2.writeToFile();
     }
 }
