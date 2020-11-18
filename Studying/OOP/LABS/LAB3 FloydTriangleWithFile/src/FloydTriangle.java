992d6783e31e2f2815e63f1f29377ec26c9f6155
Blabolka <klochko.andrew.official@gmail.com>
2020-11-18 22:42:23 +0200

    Created project LAB3 OOP

---
+++ Studying/OOP/LABS/LAB3 FloydTriangleWithFile/src/FloydTriangle.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class FloydTriangle {
+
+    private int numberOfRows;
+    private List<Integer> lazyCaterersSequence;
+    private List<Integer> triangularNumbers;
+    private List<Integer> sequence;
+
+    public FloydTriangle(int numberOfRows){
+        this.numberOfRows = numberOfRows;
+        lazyCaterersSequence = calculateLazyCaterersSequence();
+        triangularNumbers = calculateTriangularNumbers();
+        sequence = calculateSequence();
+    }
+
+    private List<Integer> calculateLazyCaterersSequence(){
+        List<Integer> lazyCaterersSequence = new ArrayList<>();
+        for (int i = 0; i < numberOfRows; i++) {
+            lazyCaterersSequence.add(getCentralPolygonalNumber(i));
+        }
+        return lazyCaterersSequence;
+    }
+
+    private List<Integer> calculateTriangularNumbers(){
+        List<Integer> triangularNumbers = new ArrayList<>();
+        for (int i = 1; i <= numberOfRows; i++) {
+            triangularNumbers.add(getTriangularNumber(i));
+        }
+        return triangularNumbers;
+    }
+
+    private List<Integer> calculateSequence(){
+        List<Integer> sequence = new ArrayList<>();
+        for (int i = 0; i < numberOfRows; i++) {
+            for (int j = lazyCaterersSequence.get(i); j <= triangularNumbers.get(i); j++) {
+                sequence.add(j);
+            }
+        }
+        return sequence;
+    }
+
+    private int getCentralPolygonalNumber(int row){
+        return (row*row + row + 2) / 2;
+    }
+
+    private static int getTriangularNumber(int row){
+        return (row*(row + 1)) / 2;
+    }
+
+}
